be83b592cd47d5c138df1103d42535eb58c0c563
fraserws <fraserws.uk2@gmail.com>
2023-02-02 19:18:08 +0000

    displaying rss

--- news/urls.py
+++ news/urls.py
@@ -4,7 +4,9 @@ from . import views
 
 urlpatterns = [
     path("__reload__/", include("django_browser_reload.urls")),
-    re_path(r'^$', views.article_list, name='article_list'),
+    re_path(r'^$', views.article_list, name='articles_list'),
+    re_path(r'^feeds/new', views.new_feed, name='feed_new'),
+    re_path(r'^feeds/', views.feeds_list, name='feeds_list'),
 
 
 ]
--- news/views.py
+++ news/views.py
@@ -1,7 +1,46 @@
 from django.shortcuts import render
-from .models import Article
+from .models import Article, Feed
+from .forms import FeedForm
+from django.shortcuts import redirect
+
+
+import feedparser as fp
+import datetime as dt
 
 
 def article_list(request):
     articles = Article.objects.all()
-    return render(request, 'news/article_list.html', {'articles': articles})
+    rows = [articles[x:x+1] for x in range(0, len(articles), 1)]
+    return render(request, 'news/articles_list.html', {'rows': rows})
+
+
+def feeds_list(request):
+    feeds = Feed.objects.all()
+    return render(request, 'news/feeds_list.html', {'feeds': feeds})
+
+
+def new_feed(request):
+    if request.method == "POST":
+        form = FeedForm(request.POST)
+        if form.is_valid():
+            feed = form.save(commit=False)
+
+            feedData = fp.parse(feed.url)
+            feed.title = feedData.feed.title
+            feed.save()
+            for entry in feedData.entries:
+                article = Article()
+                article.title = entry.title
+                article.url = entry.link
+                article.description = entry.description
+                d = dt.datetime(*entry.published_parsed[:6])
+                article.publication_date = d.strftime('%Y-%m-%d %H:%M:%S')
+
+                article.feed = feed
+
+                article.save()
+            return redirect('feeds_list')
+
+    else:
+        form = FeedForm()
+    return render(request, 'news/new_feed.html', {'form': form})
